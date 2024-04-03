966badf068039b5a940a3a167e9ed24c18c869a4
ashupednekar <ashupednekar49@gmail.com>
2024-04-03 22:54:48 +0530

    testing

---
+++ README.md
@@ -0,0 +1,1 @@
+This is where all generated grpc pb wrappers will live
---
+++ setup.py
@@ -0,0 +1,16 @@
+
+from setuptools import setup
+
+setup(
+    name="grpc_assets",
+    version="0.1.0",
+    description="grpc assets library",
+    license="MIT",
+    classifiers=[
+        "Intended Audience :: fastgrpc lib
+    ],
+    packages=["grpc_assets"],
+    scripts=[],
+    include_package_data=True,
+)
+        
